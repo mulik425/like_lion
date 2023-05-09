1fb059b784d30cc6e42715e87184da568698653b
gahyeon <kimgahyun9422@gmail.com>
2023-05-09 23:48:00 +0900

    4주차 과제

---
+++ 14/dom.js
@@ -0,0 +1,12 @@
+// var el = document.getElementById("brand-title");
+
+// console.log(el);
+
+// console.log(el.innerHTML);
+// console.log(el.innerText);
+
+// el.innerText = "안녕하세요 :)"
+
+var el = document.getElementsByClassName('sub-title');
+
+console.log(el);
